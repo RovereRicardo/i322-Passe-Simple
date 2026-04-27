f759eac357da62c1653bc256d31dc9a839f00275
Ricardo Gomes <ricardo.gomes@liip.ch>
2026-04-27 10:07:53 +0200

    Persistent Filters for recipes

--- src/app/shared/search-bar/search-bar.component.ts
+++ src/app/shared/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, output, signal } from '@angular/core';
+import { Component, input, OnInit, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -11,9 +11,14 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss',
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnInit {
+  readonly initialQuery = input<string>('');
   readonly searchQuery = signal('');
   readonly searchChange = output<string>();
+
+  ngOnInit(): void {
+    this.searchQuery.set(this.initialQuery());
+  }
 
   onQueryChange(value: string): void {
     this.searchQuery.set(value);
